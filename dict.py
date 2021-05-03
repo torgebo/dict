189fea5022e82cdc3ae036526a1fa2d2a4722abc
Torgeir Børresen <torgebo@fastmail.fm>
2021-05-03 19:29:31 +0000

    Major improvements.
    - Usage instructions
    - Bugfixes
    - log command
    - add long commands.

--- dict.py
+++ dict.py
@@ -1,46 +1,72 @@
 #!/usr/bin/env python3
+'''Dict application.
+'''
+from collections import OrderedDict
 from typing import Optional
 from os import path
 import datetime
 import os
 import re
-import readline
-import sqlite3
+import readline as _
 import sys
 
 
-HOME_DIR = os.environ["HOME"]
+HOME_DIR = os.environ['HOME']
 
-AUTHOR: str = "Torgeir Børresen"
-LOG_FILE = path.join(HOME_DIR, "dict.log")
+AUTHOR: str = 'Torgeir Børresen'
+LOG_FILE = path.join(HOME_DIR, 'dict.log')
 
 
 def show_author_subject():
-    """Display author and subject."""
-    print("Author:", AUTHOR)
+    '''Display author and subject.'''
+    print('Author:', AUTHOR)
     subject = get_last_subject()
-    print("Subject:", subject)
+    print('Subject:', subject)
+
+
+def show_available_commands(file=sys.stdout, isatty=False):
+    '''Display program commands on file.
+
+    If isatty, use colors.
+    '''
+    out_fmt = '\t{col_emph:s}{cmd:{cmd_width:d}s}{col_normal:s}: {usage:20s}\n'
+    heightened = '\u001b[31m' if isatty else ''
+    reset = '\u001b[0m' if isatty else ''
+    commands = OrderedDict([
+        ('l[og]', 'dict l'),
+        ('w[rite]', 'dict w'),
+        ('s[earch]', 'dict s `regexp`'),
+    ])
+    cmd_str = ''
+    cmd_width = max((len(c) for c in commands.keys()))
+    for cmd, usage in commands.items():
+        cmd_str += out_fmt.format(
+            col_emph=heightened, col_normal=reset,
+            cmd=cmd, cmd_width=cmd_width + 1, usage=usage,
+        )
+    print('Usage:', file=file)
+    print(cmd_str, file=file, end='')
 
 
 def get_last_subject() -> Optional[str]:
-    """Get last subject."""
+    '''Get last subject.'''
     line: Optional[str] = None
     if not path.exists(LOG_FILE):
         return None
-    with open(LOG_FILE, "r") as f:
+    with open(LOG_FILE, 'r') as f:
         for line in f:
             continue
     if line is None:
         return None
-    entries = line.strip().split("\t")
+    entries = line.strip().split('\t')
     return entries[1]
 
 
 def log_write():
-    """Emit log."""
+    '''Emit log.'''
     timestamp: str = datetime.datetime.now().isoformat()
     default_subject: Optional[str] = get_last_subject()
-    subject_msg = "subject (default: {default:s}): ".format(
+    subject_msg = 'subject (default: {default:s}): '.format(
         default=str(default_subject))
     subject: str = ''
     while not subject:
@@ -50,16 +76,16 @@ def log_write():
 
     log_entry: str = ''
     while not log_entry:
-        log_entry = input("log: ").strip()
-    with open(LOG_FILE, "a") as f:
-        print(timestamp, subject, log_entry, sep="\t", file=f)
+        log_entry = input('log: ').strip().replace('\t', ' ')
+    with open(LOG_FILE, 'a') as f:
+        print(timestamp, subject, log_entry, sep='\t', file=f)
 
 
-def log_search(filename, regex, isatty=False):
-    heightened = "\u001b[31m" if isatty else ""
-    reset = "\u001b[0m" if isatty else ""
+def log_search(log_fn, regex, out_buf, isatty=False):
+    heightened = '\u001b[31m' if isatty else ''
+    reset = '\u001b[0m' if isatty else ''
     exp = re.compile(regex)
-    with open(filename, "r") as f:
+    with open(log_fn, 'r') as f:
         for line in f:
             match = exp.search(line)
             if match:
@@ -70,18 +96,33 @@ def log_search(filename, regex, isatty=False):
                     heightened, line[s:e], reset,
                     line[e:],
                     sep='', end='',
+                    file=out_buf,
                 )
 
 
-if __name__ == "__main__":
+def display_log(buf, isatty=False):
+    default_subject: Optional[str] = get_last_subject()
+    if not default_subject:
+        sys.exit(0)
+    regexp = '\t' + default_subject + '\t'
+    log_search(LOG_FILE, regexp, buf, isatty)
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    tty = sys.stdout.isatty()
     if len(sys.argv) == 1:
         show_author_subject()
-        exit(0)
-    elif sys.argv[1] == "w":
+        print()
+        show_available_commands(file=sys.stdout, isatty=tty)
+        sys.exit(0)
+    elif sys.argv[1] in ('w', 'write'):
         log_write()
-        exit(0)
-    elif sys.argv[1] == "s":
-        if len(sys.argv) < 2:
-            print("usage: dict s `regexp`", file=sys.stderr)
-            exit(1)
-        log_search(LOG_FILE, sys.argv[2], sys.stdout.isatty())
+        sys.exit(0)
+    elif sys.argv[1] in ('s', 'search'):
+        if len(sys.argv) < 3:
+            show_available_commands(file=sys.stderr, isatty=tty)
+            sys.exit(1)
+        log_search(LOG_FILE, sys.argv[2], sys.stdout, tty)
+    elif sys.argv[1] in ('l', 'log'):
+        display_log(buf=sys.stdout, isatty=tty)
